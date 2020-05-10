5fa394067da0fd67e478fddf460cea0de252be36
ananya pandey <ananyapa@ananyas-MacBook-Pro.local>
2020-05-10 22:43:16 +0530

    ggplot viz bivariate quantitative

--- R_Programming/DataVisualization/DataVis.R
+++ R_Programming/DataVisualization/DataVis.R
@@ -1,84 +1,59 @@
-df = data.frame(name=c('a','b','c'),value=c(2,5,7))
-df
+# preparing the data 
+setwd("/Users/ananyapa/Gdrive/LEARNING/R_Programming/DataVisualization")
+movies <- read.csv("movies.csv", header = TRUE)
 
-plot(df) # X and y AXIS lablled, Here we are passing Column
-plot(df$name,df$value) # Not Lablled. Here we are passing vectors
+# changing names for convenience 
+names(movies)[names(movies) == "Runtime..Minutes."] <- "Runlength"
+names(movies)[names(movies) == "Revenue..Millions."] <- "Revenue"
 
-# plot(XComponent,Ycomponent)
+# REmoving unnecessary data 
+movies$Director = NULL
 
-plot(x = df$name, y = df$value) # Here we are passing vectors
-
-# one vector is string another is numeric
-# therefore only one component is numeric
-
-barplot(
-  names = df$name,
-  height = df$value,
-  col = "skyblue",
-  main = "PLotting heights",
-  xlab = "Name",
-  ylab = "Heights"
+plot(
+  x = movies$Runlength,
+  y = movies$Revenue,
+  main = "Plotting Bivariate Quantitative",
+  xlab = "Revenue (Millions)",
+  ylab = "Movie LEngth (Minutes)"
 )
 
-?plot  # More help on plotting BASE graphics system
-?barplot
+model <- lm(movies$Revenue ~ movies$Runlength)
 
-## Producing the same graph in Lattics. 
-install.packages("lattice")
-library(lattice)
+lines(x = movies$Runlength, y = model$fitted,
+      col = "red",
+      lwd = 3)
 
-dotplot(x=value ~ name, data = df)
-# Lattice uses formula to understand what should be plotted. 
-# This is read as value as a function of name or Y as a function of X
-# Y = f(X)
+install.packages("hexbin")
+library(hexbin)
+hexbin <- hexbin(
+  x = movies$Runlength,
+  y = movies$Revenue,
+  xbins = 30,
+  xlab = "Movie Length",
+  ylab = "Revenue"
+)
 
-barchart(x=value ~ name, data = df,
-        main = "Hello Lattice",
-        col="blue",
-        xlab = "Name",
-        ylab = "Height")
-# Bars thin, Fonts Smaller, Border around chart 
-?barchart
+plot(hexbin, main = "Plotting using Hexbin")
 
-###################### GGPLOT2 ####################
+# Preparation for Contour Plot 
+# Creating the density from data 
+install.packages("MASS")
+library(MASS)
+density2d <- kde2d(
+  x = movies$Runlength,
+  y = movies$Revenue,
+  n = 50)
 
-install.packages("ggplot2",dependencies = TRUE)
-library(ggplot2); # suppress the output 
+# Creating the Contour Plot 
+contour(
+  x = density2d$x,
+  y = density2d$y,
+  z = density2d$z,
+  main = "Plotting Contour",
+  xlab = "Movies Runtime",
+  ylab = "Movies Revenue")
 
-ggplot(data = df,
-       aes(x = name, y = value)) +
-  geom_point() # it can be point line bar etc
-
-## adding other attributes
-## using + we can add and chain multiple visual elements / Layers
-
-ggplot(data = df,
-       aes(x = name, y = value)) +
-  geom_bar(
-    stat = "identity",
-    fill = "skyblue") + 
-  ggtitle("Plotting name height ggplot") + 
-  xlab("Name") +
-  ylab("Height")
-# This one has broad bars, light grey background 
-
-??ggplot2
-?geom_bar
-
-# Base is command oriented and detail oriented keep adding details to the plot
-# Observations : ROWS
-# VARIABLES / FEATURES : COLUMNS
-
-# Qualitative - Categorical / Nominal 
-# Quantitateive - Numeric / Measure : Discrete or Continous
-
-# Qualitative : Univariate analysis
-# Qualitative : numeric analysis
-
-# Quantitative : univariate / bi variate / multivariate analysis 
-
-
-
-
-
-
+# Creating a Level Plot 
+image(x = density2d$x,
+      y = density2d$y,
+      z = density2d$z)
